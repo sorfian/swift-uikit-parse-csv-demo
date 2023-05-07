bac5a214384636201c0367e66574038207011c01
Sorfian <sorfian.tkj@gmail.com>
2023-05-07 11:02:34 +0800

    add all files

--- Parsing CSV Demo/AppDelegate.swift
+++ Parsing CSV Demo/AppDelegate.swift
@@ -15,6 +15,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        preloadData()
+        
         return true
     }
 
@@ -79,3 +82,121 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 }
 
+extension AppDelegate {
+    func parseCSV (contentsOfURL: URL, encoding: String.Encoding) -> [(name:String, detail:String, price: String)]? {
+        
+        // Load the CSV file and parse it
+        let delimiter = ","
+        var items:[(name:String, detail:String, price: String)]?
+        
+        do {
+            let content = try String(contentsOf: contentsOfURL, encoding: encoding)
+            items = []
+            let lines: [String] = content.components(separatedBy: .newlines)
+            
+            for line in lines {
+                var values:[String] = []
+                if line != "" {
+                    // For a line with double quotes
+                    // we use NSScanner to perform the parsing
+                    if line.range(of: "\"") != nil {
+                        var textToScan: String = line
+                        var value: String?
+                        var textScanner:Scanner = Scanner(string: textToScan)
+                        while textScanner.string != "" {
+                            
+                            if (textScanner.string as NSString).substring(to: 1) == "\"" {
+                                textScanner.currentIndex = textScanner.string.index(after: textScanner.currentIndex)
+                                value = textScanner.scanUpToString("\"")
+                                textScanner.currentIndex = textScanner.string.index(after: textScanner.currentIndex)
+                            } else {
+                                value = textScanner.scanUpToString(delimiter)
+                            }
+                            
+                            // Store the value into the values array
+                            if let value = value {
+                                values.append(value)
+                            }
+                            
+                            // Retrieve the unscanned remainder of the string
+                            if !textScanner.isAtEnd {
+                                let fromIndex = textScanner.string.index(after: textScanner.currentIndex)
+                                textToScan = String(textScanner.string.suffix(from: fromIndex))
+
+                            } else {
+                                textToScan = ""
+                            }
+                            textScanner = Scanner(string: textToScan)
+                        }
+                        
+                        // For a line without double quotes, we can simply separate the string
+                        // by using the delimiter (e.g. comma)
+                    } else  {
+                        values = line.components(separatedBy: delimiter)
+                    }
+                    
+                    // Put the values into the tuple and add it to the items array
+                    let item = (name: values[0], detail: values[1], price: values[2])
+                    items?.append(item)
+                    
+                }
+            }
+            
+        } catch {
+            print(error)
+        }
+        
+        return items
+    }
+    
+    func preloadData() {
+        
+        // Load the data file from a remote URL
+        guard let remoteURL = URL(string: "https://drive.google.com/uc?export=download&id=0ByZhaKOAvtNGelJOMEdhRFo2c28") else {
+            return
+        }
+        
+        // Remove all the menu items before preloading
+        removeData()
+        
+        // Parse the CSV file and import the data
+        if let items = parseCSV(contentsOfURL: remoteURL, encoding: String.Encoding.utf8) {
+            
+            let context = persistentContainer.viewContext
+            
+            for item in items {
+                let menuItem = MenuItem(context: context)
+                menuItem.name = item.name
+                menuItem.detail = item.detail
+                menuItem.price = Double(item.price) ?? 0.0
+                
+                do {
+                    try context.save()
+                } catch {
+                    print(error)
+                }
+            }
+            
+        }
+    }
+    
+    func removeData() {
+        // Remove the existing items
+        let fetchRequest = NSFetchRequest<MenuItem>(entityName: "MenuItem")
+        let context = persistentContainer.viewContext
+
+        do {
+
+            let menuItems = try context.fetch(fetchRequest)
+        
+            for menuItem in menuItems {
+                context.delete(menuItem)
+            }
+            
+            saveContext()
+            
+        } catch {
+            print(error)
+        }
+    }
+}
---
+++ Parsing CSV Demo/MenuTableViewController.swift
@@ -0,0 +1,60 @@
+//
+//  MenuTableViewController.swift
+//  Parsing CSV Demo
+//
+//  Created by Sorfian on 28/04/23.
+//
+
+import UIKit
+import CoreData
+
+class MenuTableViewController: UITableViewController {
+    
+    private var menuItems:[MenuItem] = []
+    var fetchResultController: NSFetchedResultsController<MenuItem>!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load menu items from database
+        if let appDelegate = (UIApplication.shared.delegate as? AppDelegate) {
+            let request: NSFetchRequest<MenuItem> = MenuItem.fetchRequest()
+            let context = appDelegate.persistentContainer.viewContext
+            do {
+                menuItems = try context.fetch(request)
+            } catch {
+                print("Failed to retrieve record")
+                print(error)
+            }
+        }
+        
+        // Make the cell self size
+        tableView.estimatedRowHeight = 66.0
+        tableView.rowHeight = UITableView.automaticDimension
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return menuItems.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! MenuTableViewCell
+
+        // Configure the cell...
+        cell.nameLabel.text = menuItems[indexPath.row].name
+        cell.detailLabel.text = menuItems[indexPath.row].detail
+        cell.priceLabel.text = "$\(menuItems[indexPath.row].price)"
+
+        return cell
+    }
+
+
+}
